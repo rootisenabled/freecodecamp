b996d664a0a4bfb464c6a97c6d1cdd616641feca
Alex Ivanov <saniaivanov94@gmail.com>
2017-01-13 23:22:24 +0100

    Finished the app

--- Show the local weather/main.js
+++ Show the local weather/main.js
@@ -1,40 +1,61 @@
-function fetchData(url) {
-    return new Promise((resolve, reject) => {
-
-        const xhr = new XMLHttpRequest();
-        xhr.open('GET', url);
-
-        xhr.onload = () => {
-            (xhr.status == 200) ? resolve(xhr.response): reject(Error(xhr.statusText));
-        };
-
-        xhr.onerror = () => {
-            reject(Error('Network Error!'));
-        }
-
-        xhr.send();
-    })
-}
-
-fetchData('http://ip-api.com/json')
+fetch('http://ip-api.com/json')
     .then(response => {
-        console.log(response);
-        let location = JSON.parse(response);
-        return location;
+        return response.json().then(data => data);
     })
     .then(location => {
-        const lat = location.lat;
-        const lon = location.lon;
-        return fetchData(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=9127c9c34a42acd7f89180689aa55b9f`);
+        const { city } = location;
+        return fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=9127c9c34a42acd7f89180689aa55b9f&units=metric`);
     })
     .then(response => {
-        const weather = JSON.parse(response);
-        console.log(weather);
+        response.json().then(weatherData => {
 
-        let temp = document.getElementById("temp");
-        temp.innerHTML = toCelcius(weather.main.temp);
+            let tempContainer = document.getElementById("temp");
+            let weatherContainer = document.querySelector('.description');
+            let locationContainer = document.querySelector('.location');
+            let dateContainer = document.querySelector('.date-today');
+            let windSpeedContainer = document.querySelector('.wind-speed');
+            let humidityContainer = document.querySelector('.humidity');
+
+            let { main, description } = weatherData.weather.find(elem => elem);
+            let { speed } = weatherData.wind;
+            let {temp, humidity } = weatherData.main;
+
+            tempContainer.innerHTML = temp.toFixed();
+            weatherContainer.innerHTML = description;
+            locationContainer.innerHTML = weatherData.name;
+            windSpeedContainer.innerHTML = `${speed} MPH`;
+            humidityContainer.innerHTML = `${humidity} %`
+
+            let date = new Date(),
+                locale = "en-us",
+                month = date.toLocaleString(locale, { month: "long" });
+            dateContainer.innerHTML = `${month} <br /> ${date.getDate()}`;
+            setWeatherIcon(`img/weather-${main.toLowerCase()}.png`);
+        });
     });
 
+
+const units = document.getElementById('units');
+units.addEventListener('click', (e) => {
+    const tempValue = units.previousSibling;
+    if (units.innerHTML !== 'F') {
+        tempValue.innerHTML = toFarenheit(tempValue.innerHTML).toFixed(0);
+        units.innerHTML = 'F';
+    } else {
+        tempValue.innerHTML = toCelcius(tempValue.innerHTML).toFixed(0);
+        units.innerHTML = '&deg;C';
+    }
+})
+
+function toFarenheit(temp) {
+    return (9 * temp) / 5 + 32;
+}
+
 function toCelcius(temp) {
-    return (temp - 272.15).toFixed(1);
+    return (temp - 32) * 5 / 9;
+}
+
+function setWeatherIcon(weatherType) {
+    const icon = document.getElementById('weather-icon');
+    icon.setAttribute('src', weatherType);
 }
